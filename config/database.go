566a27e064c0b99c77fb0efea77db1930f2e8eb6
Alfiqo Jundullah <phicko93@gmail.com>
2023-07-19 22:20:28 +0700

    fix config database typo db password

--- config/database.go
+++ config/database.go
@@ -19,7 +19,7 @@ func NewDB() *gorm.DB {
 
 	dbHost := os.Getenv("DB_HOST")
 	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWROD")
+	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbPort := os.Getenv("DB_PORT")
 
